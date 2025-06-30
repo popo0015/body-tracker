f5d341828b71ad3acf424c1fa1aefca14082997d
popo0015 <popo0015@hz.nl>
2025-06-30 18:18:00 +0200

    Vite config for host approval

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  server: process.env.NODE_ENV === 'development' ? { host: 'localhost', port: 5173 } : undefined
+  server: process.env.NODE_ENV === 'development' ? { host: 'localhost', port: 5173 } : undefined,
+  preview: {
+    allowedHosts: ['body-tracker-ocjq.onrender.com']
+  }
 });
